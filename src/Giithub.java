9157ae11a24590ea9b1875f39fc48cdd679f9385
Krefeld <60035444+Krefeld@users.noreply.github.com>
2020-09-13 17:37:04 +0200

    tesst

--- src/Giithub.java
+++ src/Giithub.java
@@ -71,12 +71,6 @@ public class Giithub {
 		selectionsort(arr2);
 		ausgeben(arr2);
 		
-		System.out.println();
-		System.out.println("Quicksort");
-		quickSort(arr3,0,arr3.length-1);
-		ausgeben(arr3);
-		
-		
 	}
 	
 	static public void kopieren(int[] feld, int[] feld1, int[] feld2, int [] feld3)
@@ -315,57 +309,4 @@ public class Giithub {
 	       }
 	       return minIndex; 
 	}
-	
-	public static void quickSort(String[] a, int start, int end)
-	{
-	        // index for the "left-to-right scan"
-	        int i = start;
-	        // index for the "right-to-left scan"
-	        int j = end;
-
-	        // only examine arrays of 2 or more elements.
-	        if (j - i >= 1)
-	        {
-	            // The pivot point of the sort method is arbitrarily set to the first element int the array.
-	            String pivot = a[i];
-	            // only scan between the two indexes, until they meet.
-	            while (j > i)
-	            {
-	                // from the left, if the current element is lexicographically less than the (original)
-	                // first element in the String array, move on. Stop advancing the counter when we reach
-	                // the right or an element that is lexicographically greater than the pivot String.
-	                while (a[i].compareTo(pivot) < 0 && i <= end && j > i){
-	                    i++;
-	                }
-	                // from the right, if the current element is lexicographically greater than the (original)
-	                // first element in the String array, move on. Stop advancing the counter when we reach
-	                // the left or an element that is lexicographically less than the pivot String.
-	                while (a[j].compareTo(pivot) > 0 && j >= start && j >= i){
-	                    j--;
-	                }
-	                // check the two elements in the center, the last comparison before the scans cross.
-	                if (j > i)
-	                    swap1(a, i, j);
-	            }
-	            // At this point, the two scans have crossed each other in the center of the array and stop.
-	            // The left partition and right partition contain the right groups of numbers but are not
-	            // sorted themselves. The following recursive code sorts the left and right partitions.
-
-	            // Swap the pivot point with the last element of the left partition.
-	            swap1(a, start, j);
-	            // sort left partition
-	            quickSort(a, start, j - 1);
-	            // sort right partition
-	            quickSort(a, j + 1, end);
-	        }
-	    }
-	    /**
-	     * This method facilitates the quickSort method's need to swap two elements, Towers of Hanoi style.
-	     */
-		public static void swap1(String[] a, int i, int j)
-	    {
-	    	String temp = a[i];
-	    	a[i] = a[j];
-	    	a[j] = temp;
-	    }
 }
